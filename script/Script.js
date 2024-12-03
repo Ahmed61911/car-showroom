f38a1fbb1b5999e8c0e8fa849dfff9a1f7099540
Ahmed Baba <ahmed.baba.ab.96@gmail.com>
2024-12-04 00:59:38 +0100

    first commit of the project

---
+++ script/Script.js
@@ -0,0 +1,12 @@
+
+
+//Localistion script
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(34.658190714995385, -1.9079123613678424),
+    zoom:15,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
+
+  //header bar script
